3b26020c6d988051661225f2e4749ef1745bef90
Fenil Desai <feniludesai@yahoo.com>
2020-03-26 10:15:29 +0000

    Update data.js

--- data/data.js
+++ data/data.js
@@ -7,68 +7,72 @@
 // Change your profile data here
 let profile = {
   picture: "",
-  name: "Guim González",
-  description: "Software Engineer & Fullstack Developer",
+  name: "Fenil Desai",
+  description: "Web Developer",
   bio:
-    "I'm a Software Engineer interested in Internet technologies, computer networks and security. From the FullStack development to the most innovative technologies such as Blockchain.<br><br>High level of web languages (JS, HTML and CSS), average level of C and C++. Great understanding of Internet protocols, P2P networks and cloud infrastructures. I work with JS frameworks, JQuery (old days) and React.js (now).",
-  email: "guim.gonzalez@gmail.com",
-  location: "Barcelona, Spain",
-  birth: "1997-07-26", // YYYY-MM-DD format
+    "I'm a Web Developer interested in Web technologies. Aspiring to become a FullStack developer <br><br>High level of web languages (ASP.NET, C#, SQL Server, Azure DevOps, Jquery, JS, HTML and CSS). I work with ASP.NET, Visual Studio 2019, .NET Core, Azure DevOps (now).",
+  email: "feniludesai@yahoo.com",
+  location: "Pune, India",
+  birth: "1984-10-30", // YYYY-MM-DD format
   skills: [
-    "Blockchain",
-    "Bitcoin",
-    "C/C++",
-    "CSS",
-    "Ethereum",
+    "ASP.NET",
+    "C#",
+    ".NET Core",
+    "Azure DevOps",
     "HTML",
     "Javascript",
-    "MongoDB",
-    "NodeJS",
-    "React",
-    "Solidity",
     "SQL",
-    "Web3"
   ],
   social: {
-    linkedin: "https://www.linkedin.com/in/guim-gonzalez-846606134/",
-    twitter: "https://twitter.com/GuimGonzalez",
-    github: "https://github.com/GuimG/",
-    stackoverflow: "https://stackoverflow.com/users/8700961/guim-gonzalez",
-    dev: "https://dev.to/guimg"
+    linkedin: "https://www.linkedin.com/in/fenildesai/",
+    twitter: "https://twitter.com/fenildesai",
+    github: "https://github.com/fenildesai/",
+    stackoverflow: "https://stackoverflow.com/users/4032748/dreambig",
+    dev: "https://dev.to/fenildesai"
   },
   experience: [
     {
-      title: "FullStack Developer",
-      company: "Freelance",
-      url: "https://guimgonzalez.business/",
-      location: "Barcelona, Spain",
+      title: "Associate Consultant",
+      company: "Tata Consultancy Services",
+      url: "https://www.tcs.com/",
+      location: "Pune, India",
       description:
-        "I do websistes for small businesses. I develop both frontend and backend. I also setup and mantain the VPS.",
-      from: "September 2017",
+        "Working for one of the top UK insurer in building web applications for their quote & buy journeys.",
+      from: "Jan 2011",
       to: "Now"
     },
     {
-      title: "IT Consultant",
-      company: "Innova-TSN",
-      url: "https://www.innova-tsn.com/",
-      location: "Barcelona, Spain",
+      title: "Web Developer",
+      company: "Synechron",
+      url: "https://www.synechron.com/",
+      location: "Pune, India",
       description:
-        "I developed web services, worked with SAS (ETL's), and email marketing with Oracle Eloqua.",
-      from: "July 2018",
-      to: "February 2019"
+        "I worked in R&D Department building POCs for gaining new business/projects specifically in the Social Media space - like building Facebook & Twitter applications & also worked on building Augmented Reality applications.",
+      from: "June 2010",
+      to: "Jan 2011"
+    },
+    {
+      title: "Web Developer",
+      company: "Agribuys",
+      url: "https://www.foodlinkonline.com/",
+      location: "Pune, India",
+      description:
+        "I was part of the Product Development Team for developing & designing various modules of the live web based system of FoodlinkOnline.com - a Complete Supply chain management portal for connecting buyers and sellers of fresh food and floral products.",
+      from: "Jan 2008",
+      to: "June 2010"
     }
   ],
   education: [
     {
-      school: "Universitat Autònoma de Barcelona",
-      degree: "Engineering",
-      fieldOfStudy: "Computer Science",
-      location: "Barcelona, Spain",
-      from: "September 2016",
-      to: "Now"
+      school: "South Gujarat University, Surat",
+      degree: "Masters in Computer Applications",
+      fieldOfStudy: "Computers",
+      location: "Bharuch, Gujarat, India",
+      from: "2005",
+      to: "2008"
     }
   ],
-  quote: "I turn coffee into code"
+  quote: "A cricketer by heart & a developer by mistake"
 };
 
 let page = {
